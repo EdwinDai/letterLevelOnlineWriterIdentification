f5401f0d0374624c0e83e2aef5559aee47ee8a86
EdwinMac <2946565956@qq.com>
2023-03-08 10:39:04 +0800

    update

--- test.py
+++ test.py
@@ -33,31 +33,32 @@ train_dataset, test_dataset = random_split(
     lengths=[25600, 6400],
     generator=torch.Generator().manual_seed(1)
 )
-train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=2, shuffle=False)
+train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False)
 
 model = NeuralNetwork()
-# loss_fn = nn.CrossEntropyLoss().cuda()
-# learning_rate = 0.001
-# optimizer = torch.optim.Adam(model.parameters())
+loss_fn = nn.CrossEntropyLoss().cuda()
+learning_rate = 0.001
+optimizer = torch.optim.Adam(model.parameters())
 
 epoch = 20
 
-# for i in range(epoch):
-#     train(train_dataloader, model, loss_fn, optimizer, writer=writer, currentEpoch=i)
-#     test(test_dataloader, model, loss_fn, currentEpoch=i, writer=writer)
+for i in range(epoch):
+    train(train_dataloader, model, loss_fn, optimizer, writer=writer, currentEpoch=i)
+    test(test_dataloader, model, loss_fn, currentEpoch=i, writer=writer)
 
 # for x, y in train_dataloader:
-    # x1, x2 = x
-#     # print(x1.type)
-#     # print(x2.shape)
-#     res = model(x)
-#     print(res.shape)
-#     # print(res)
-#     # print(y.shape)
+#     x1, x2 = x
+#     print(len(x))
+#     print(x1.shape)
+#     print(x2.shape)
+# #     res = model(x)
+# #     print(res.shape)
+# #     # print(res)
+#     print(y.shape)
 #     break
 # paras = model.parameters()
-total = sum([param.nelement() for param in model.parameters()])
-
-print("Number of parameter: %.2fM" % (total / 1e6))
+# total = sum([param.nelement() for param in model.parameters()])
+#
+# print("Number of parameter: %.2fM" % (total / 1e6))
 
