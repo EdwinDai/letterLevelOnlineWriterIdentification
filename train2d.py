4f2f111af2687619928c53802866706443561b1e
EdwinDai <edwindai@foxmail.com>
2023-05-09 03:22:42 +0800

    update

--- train2d.py
+++ train2d.py
@@ -2,7 +2,7 @@ import torch
 import os
 
 
-def train(dataloader, model, loss_fn, optimizer, currentEpoch):
+def train(dataloader, model, loss_fn, optimizer, writer, currentEpoch):
     model.train()
     print('Current Epoch:', currentEpoch)
     correct = 0
@@ -16,29 +16,26 @@ def train(dataloader, model, loss_fn, optimizer, currentEpoch):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        print('loss', loss)
-        print('pred', pred)
         num_equal = torch.eq(pred, y).sum().item()
-        print('num_equal', num_equal)
         correct += num_equal
         if batch % 252 == 0:
             loss, current = loss.item(), (batch + 1) * 32
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
     correct /= size
     print(f"Train Error: \n Accuracy: {(100 * correct):>0.01f}%")
-    # writer.add_scalar(tag="loss/train",
-    #                   scalar_value=loss,
-    #                   global_step=currentEpoch
-    #                   )
-    # writer.add_scalar(tag="accuracy/train",
-    #                   scalar_value=correct * 100,
-    #                   global_step=currentEpoch
-    #                   )
+    writer.add_scalar(tag="loss/train",
+                      scalar_value=loss,
+                      global_step=currentEpoch
+                      )
+    writer.add_scalar(tag="accuracy/train",
+                      scalar_value=correct * 100,
+                      global_step=currentEpoch
+                      )
     # path = os.path.join(r'E:\file\Code\deepLearning\signature', currentEpoch + '.pth')
     # torch.save(model.state_dict(), path)
 
 
-def test(dataloader, model, loss_fn, currentEpoch):
+def test(dataloader, model, loss_fn, writer, currentEpoch):
     model.eval()
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -54,12 +51,12 @@ def test(dataloader, model, loss_fn, currentEpoch):
     test_loss /= num_batches
     correct /= size
 
-    # writer.add_scalar(tag="loss/val",
-    #                   scalar_value=test_loss,
-    #                   global_step=currentEpoch
-    #                   )
-    # writer.add_scalar(tag="accuracy/val",
-    #                   scalar_value=correct * 100,
-    #                   global_step=currentEpoch
-    #                   )
+    writer.add_scalar(tag="loss/val",
+                      scalar_value=test_loss,
+                      global_step=currentEpoch
+                      )
+    writer.add_scalar(tag="accuracy/val",
+                      scalar_value=correct * 100,
+                      global_step=currentEpoch
+                      )
     print(f"Test Error: \n Accuracy: {(100 * correct):>0.01f}%, Avg loss: {test_loss:>8f} \n")
